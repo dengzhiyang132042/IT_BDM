b673cfd916ba27597bf30902793413dfb9300008
ZS1994 <1217360619@qq.com>
2016-09-14 11:25:04 +0800

    (第五次)故障系统实体类创建
    Signed-off-by: ZS1994 <1217360619@qq.com>

---
+++ IT_BDM/src/com/zs/entity/DaPerform.java
@@ -0,0 +1,92 @@
+package com.zs.entity;
+
+import java.sql.Timestamp;
+
+/**
+ * DaPerform entity. @author MyEclipse Persistence Tools
+ */
+
+public class DaPerform implements java.io.Serializable {
+
+	// Fields
+
+	private String PId;
+	private String UNum;
+	private Timestamp PTime;
+	private String PState;
+	private String UNumNext;
+	private String DId;
+
+	// Constructors
+
+	/** default constructor */
+	public DaPerform() {
+	}
+
+	/** minimal constructor */
+	public DaPerform(String PId) {
+		this.PId = PId;
+	}
+
+	/** full constructor */
+	public DaPerform(String PId, String UNum, Timestamp PTime, String PState,
+			String UNumNext, String DId) {
+		this.PId = PId;
+		this.UNum = UNum;
+		this.PTime = PTime;
+		this.PState = PState;
+		this.UNumNext = UNumNext;
+		this.DId = DId;
+	}
+
+	// Property accessors
+
+	public String getPId() {
+		return this.PId;
+	}
+
+	public void setPId(String PId) {
+		this.PId = PId;
+	}
+
+	public String getUNum() {
+		return this.UNum;
+	}
+
+	public void setUNum(String UNum) {
+		this.UNum = UNum;
+	}
+
+	public Timestamp getPTime() {
+		return this.PTime;
+	}
+
+	public void setPTime(Timestamp PTime) {
+		this.PTime = PTime;
+	}
+
+	public String getPState() {
+		return this.PState;
+	}
+
+	public void setPState(String PState) {
+		this.PState = PState;
+	}
+
+	public String getUNumNext() {
+		return this.UNumNext;
+	}
+
+	public void setUNumNext(String UNumNext) {
+		this.UNumNext = UNumNext;
+	}
+
+	public String getDId() {
+		return this.DId;
+	}
+
+	public void setDId(String DId) {
+		this.DId = DId;
+	}
+
+}
