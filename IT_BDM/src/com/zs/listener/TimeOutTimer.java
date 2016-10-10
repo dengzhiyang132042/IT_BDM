71084e0bce8ad76982b80b5dccca1ece4e1085df
ZS1994 <1217360619@qq.com>
2016-10-10 10:29:05 +0800

    监听器、定时器代码优化——张顺
    Signed-off-by: ZS1994 <1217360619@qq.com>

--- IT_BDM/src/com/zs/listener/TimeOutTimer.java
+++ IT_BDM/src/com/zs/listener/TimeOutTimer.java
@@ -57,7 +57,6 @@ public class TimeOutTimer extends TimerTask{
     	 * */
     	//邮件发送所需数据
 		Users um = null;
-		DaDemand dd =null;
 		DaPerform perform=null;
     	
     	//先得到当天开始的时间
@@ -81,46 +80,6 @@ public class TimeOutTimer extends TimerTask{
 		
 			
 	}
-
-	public static boolean outMailFromUpdate(DaPerform tmpper,DaDemand d,IService ser){
-		//添加一个给主管发送邮件，并标明为什么未完成
-		//邮件模块需要带的数据
-		Users um = (Users) ser.get(Users.class, tmpper.getUNum());
-		DaDemand dd=(DaDemand) ser.get(DaDemand.class, d.getDId());
-		//编写邮件内容
-		String content="<style type=\"text/css\">span{display:block;margin:5px 0;font-size:15px;} .table1{	border: #224466;	border-collapse:collapse;	width: 800px;} .tleft{text-align:left;}</style>";
-		content=content+"<div style=\"font-family:微软雅黑;font-size:15px;\">"+
-		"<div style=\"height:400px;width:800px;\">"+
-		"<span>Dear 徐主管</span>"+
-		"<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;您收到有"+um.getUName()+"发送给您的未完成故障详情！</span>"+
-		"<span>未完成原因:"+tmpper.getPNote()+"</span>"+
-		"<table class=\"table1\" border=\"1\">" +
-		"<tr>" +
-		"<td class=\"tleft\">编&nbsp;&nbsp; 号:</td><td>"+dd.getDId()+"</td><td class=\"tleft\">发 起 人:</td><td>"+dd.getDApplicant()+"</td></tr>" +
-		"<tr><td class=\"tleft\">故障类型:</td><td>"+dd.getDType()+"</td><td class=\"tleft\">创建时间:</td><td>"+new SimpleDate(dd.getDTime())+"</td></tr>" +
-		"<tr><td>超时时间:</td><td>"+dd.getDTimeExpect()+"</td></tr>" +
-		"</table>"+
-		"<span>故障描述:</span>"+
-		"<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+dd.getDContent()+"</span>"+
-		"</div>"+
-		"<br/>Best Wishes<br/>"+
-		"以流程为导向，以服务为宗旨。<br/>"+
-		"*****************************************************"+
-		"<br/>信息与流程管理部-客服专员 &nbsp;"+um.getUName()+""+
-		"<br/>深圳市韵达速递有限公司<br/>邮箱："+um.getUMail()+""+
-		"<br/>地址：广东省深圳市龙华新区观澜大道114号（交警中队正对面）<br/>"+
-		"***************************************************<br/></div>";
-		//邮件标题
-		String title="故障处理未完成";
-		String sj="guanghui_huang@szexpress.com.cn";
-		try {
-			MailTest.outputMail(sj,MailTest.IT_ROBOT, content, title);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
-    	 
     	
     	
 }
