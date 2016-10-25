bbd8c1beacfabd64cebf781ce1a5eafbd61b36e5
hgh <453668907@qq.com>
2016-10-25 14:28:19 +0800

    接口定义，周的算法   黄光辉
    Signed-off-by: hgh <453668907@qq.com>

--- IT_BDM/src/com/zs/service/iXtZmNumberService.java
+++ IT_BDM/src/com/zs/service/iXtZmNumberService.java
@@ -1,14 +1,26 @@
 package com.zs.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import com.zs.dao.IBaseDaoOfSpring;
 import com.zs.entity.XtZmNumber;
 
 
+
+
 public interface iXtZmNumberService{
 	
+	public static final String KEY_DATE_START="";
+	public static final String KEY_DATE_END="";
 	
 	public List initXtZmBumberService(List<XtZmNumber> list);
+	
+	/**
+	 * @author 黄光辉
+	 * <br/>传入一个时间返回该时间对应的周一到周日时间段
+	 */
+	public Map<String, Date> weekDate(Date date);
 	
 }
--- IT_BDM/src/com/zs/tools/TestCalendar.java
+++ IT_BDM/src/com/zs/tools/TestCalendar.java
@@ -1,9 +1,14 @@
 package com.zs.tools;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.taglibs.standard.lang.jstl.test.beans.PublicBean1;
 
 public class TestCalendar {
 
@@ -11,6 +16,9 @@ public class TestCalendar {
 	 * @param args
 	 * @throws ParseException 
 	 */
+	
+	
+	
 	public static void main(String[] args) throws ParseException {
 		
 		/*
@@ -51,13 +59,47 @@ public class TestCalendar {
 		*/
 		
 		//测试时间极限问题
-		Date date=new Date(2016-1900, 9-1, 20,23,59, 60);
-		System.out.println(date.toLocaleString());
+//		Date date=new Date(2016-1900, 9-1, 20,23,59, 60);
+//		System.out.println(date.toLocaleString());
+		
+		
+		
+		
+		//测试周数
+//		Date date=new Date(2015-1900, 12-1, 30);
+//		Map<String, Date> map = weekDate(date);
 		
 	}
 	
-	
-	
+//	public static final String KEY_DATE_START="";
+//	public static final String KEY_DATE_END="";
+//	
+//	public static Map<String, Date> weekDate(Date date){
+//		Calendar ca = Calendar.getInstance();//创建一个日期实例
+//		Calendar ca1 = Calendar.getInstance();
+//		Calendar ca2 = Calendar.getInstance();
+//		
+//		System.out.println(date.toLocaleString());
+//		ca.setTime(date);//实例化一个日期
+//		System.out.println(ca.getTime().toLocaleString());
+//		int a = ca.get(ca.DAY_OF_WEEK);
+//		System.out.println("当前周的第"+a+"天      day-->"+ca.getTime().getDate());
+//		//当前时间的周一
+//		System.out.println(ca.getTime().getYear()+1900);
+//		System.out.println(ca.getTime().getMonth()+1);
+//		ca1.set(ca.getTime().getYear()+1900, ca.getTime().getMonth(), ca.getTime().getDate()-a,23,59,60);
+//		ca2.set(ca.getTime().getYear()+1900, ca.getTime().getMonth(), ca.getTime().getDate()+(7-a),23,59,59);
+//		System.out.println("当前周的周一   "+ca1.getTime().toLocaleString());
+//		System.out.println("当前周的周日   "+ca2.getTime().toLocaleString());
+//		
+//		Map<String, Date> map=new HashMap<String, Date>();
+//		
+//		map.put(KEY_DATE_START, ca1.getTime());
+//		map.put(KEY_DATE_END, ca2.getTime());
+//		
+//		return map;
+//	}
+//	
 	
 	
 	
