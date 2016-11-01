a1770b040bbf8cefb308a8a80fd78dbff2f429da
ZS1994 <1217360619@qq.com>
2016-11-01 17:57:49 +0800

    IMO、邮箱统计模块，但【在职情况】没有处理，待以后处理。——张顺
    Signed-off-by: ZS1994 <1217360619@qq.com>

---
+++ IT_BDM/src/com/zs/entity/custom/ZmByNumCount.java
@@ -0,0 +1,43 @@
+package com.zs.entity.custom;
+
+import java.sql.Timestamp;
+
+public class ZmByNumCount {
+	private Timestamp sTime;
+	private Timestamp eTime;
+	private String state;//在职情况
+	private int number;
+	private int count;
+	public Timestamp getsTime() {
+		return sTime;
+	}
+	public void setsTime(Timestamp sTime) {
+		this.sTime = sTime;
+	}
+	public Timestamp geteTime() {
+		return eTime;
+	}
+	public void seteTime(Timestamp eTime) {
+		this.eTime = eTime;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+}
