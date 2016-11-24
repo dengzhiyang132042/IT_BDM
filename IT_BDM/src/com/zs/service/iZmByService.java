9b4b78ee23691e6bcbb379041d7e440bbb49c136
ZS1994 <1217360619@qq.com>
2016-11-24 16:59:33 +0800

    共3点改动：IMO邮箱、导入接口、OA： 1,IMO邮箱添加数据导入功能 2，设计了一个数据导入的公共service接口，之后的导入功能不必每个action写一个service接口。 3，OA账号登记模块添加数据导入功能。 ——张顺
    Signed-off-by: ZS1994 <1217360619@qq.com>

---
+++ IT_BDM/src/com/zs/service/iZmByService.java
@@ -0,0 +1,9 @@
+package com.zs.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+
+public interface iZmByService {
+	public void importExcelData(String fileName, File file);
+}
