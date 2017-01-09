eb1c95ff1a448be3469d8e22a629ffbf8af1e536
hgh <453668907@qq.com>
2017-01-09 17:53:52 +0800

    OA账号登记修改已完成   --黄光辉
    Signed-off-by: hgh <453668907@qq.com>

--- IT_BDM/src/com/zs/service/impl/ZmOaServiceImpl.java
+++ IT_BDM/src/com/zs/service/impl/ZmOaServiceImpl.java
@@ -1,6 +1,8 @@
 package com.zs.service.impl;
 
 import java.io.File;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -20,7 +22,15 @@ public class ZmOaServiceImpl extends BaseService implements iDataImportService{
 			List<String[]> list=ExcelImport.getDataFromExcel(fileName,file);
 			for (int i = 1; i < list.size(); i++) {
 				try {
-					ZmOaNumber oa=new ZmOaNumber("o"+NameOfDate.getNum(), transToDate(list.get(i)[0]), transToInt(list.get(i)[1]), list.get(i)[2], list.get(i)[3], list.get(i)[4], list.get(i)[5], list.get(i)[6], transToDate(list.get(i)[0]), null, list.get(i)[7], null, list.get(i)[8]);
+					String type="";
+					if(list.get(i)[7].equals("入职")){
+						type = "注册";
+					}else if(list.get(i)[7].equals("离职")){
+						type = "注销";
+					}else{
+						type="维护";
+					}
+					ZmOaNumber oa=new ZmOaNumber("o"+NameOfDate.getNum(), transToDate(list.get(i)[0]), transToInt(list.get(i)[1]), list.get(i)[2], list.get(i)[3], list.get(i)[4], list.get(i)[5], list.get(i)[6], transToDate(list.get(i)[0]), null, list.get(i)[7], null, list.get(i)[8],new Timestamp(new Date().getTime()),type,"有效",unum);
 					save(oa);
 				} catch (Exception e) {
 					log.error("数据格式错误:请注意填写的数据格式，另外不要留空，数字类型的没有就写0，文本类型的没有可以不写，时间类型的一定要写");
